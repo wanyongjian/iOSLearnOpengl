9a724333f11ca780ef0a04cc63d546ccfe988950
yongjian.wan <yongjian.wan@ecovacs.com>
2018-05-08 16:25:58 +0800

    111

--- iOSOpenGl/ViewController.m
+++ iOSOpenGl/ViewController.m
@@ -7,47 +7,45 @@
 //
 
 #import "ViewController.h"
-#import "OpenglView.h"
 #import <GPUImage.h>
-#import "macro.h"
-#import "FishEyeFilter.h"
-#import "InvertFilter.h"
-#import "CustomBrightnessFilter.h"
 
 @interface ViewController ()
-@property (nonatomic,strong) GPUImageVideoCamera *videoCamera;
-@property (nonatomic,strong) GPUImageView *imageView;
 
-@property (nonatomic,strong) GPUImageLookupFilter *filter;
-@property (nonatomic,strong) GPUImagePicture *picture;
-//@property (nonatomic,strong) UIImageView *imageView;
+@property (nonatomic, strong) GPUImageView *imageView;
+@property (nonatomic, strong) GPUImageStillCamera *camera;
+@property (nonatomic, strong) GPUImageUIElement *element;
 @end
 
 @implementation ViewController
-- (void)setupFilter
-{
-    self.imageView = [[GPUImageView alloc]initWithFrame:self.view.frame];
-    [self.view addSubview:self.imageView];
-    
-    self.filter = [[GPUImageLookupFilter alloc] init];
-    [self.filter setIntensity:0.65f];
-    [self.filter addTarget:self.imageView];
-    
-    self.picture = [[GPUImagePicture alloc] initWithImage:[UIImage imageNamed:@"source.jpg"]];
-    [self.picture addTarget:self.filter];
-    [self.picture processImage];
-    
-    GPUImagePicture *loockup = [[GPUImagePicture alloc] initWithImage:[UIImage imageNamed:@"lookup_amatorka.png"]];
-    [loockup addTarget:self.filter];
-    [loockup processImage];
-    
-}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self setupFilter];
+    self.camera = [[GPUImageStillCamera alloc]initWithSessionPreset:AVCaptureSessionPresetHigh cameraPosition:AVCaptureDevicePositionBack];
+    self.camera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    self.imageView = [[GPUImageView alloc]initWithFrame:self.view.frame];
+    [self.view addSubview:self.imageView];
+    
+    GPUImageMultiplyBlendFilter *blendFilter = [[GPUImageMultiplyBlendFilter alloc]init];
+    
+    UIView *backView = [[UIView alloc]initWithFrame:self.view.frame];
+    UIImageView *imgView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"awesomeface.jpg"]];
+    imgView.frame = CGRectMake(100, 100, 100, 100);
+    [backView addSubview:imgView];
+    self.element = [[GPUImageUIElement alloc]initWithView:backView];
+    [self.element addTarget:blendFilter];
+    //加这个直通滤镜是为了在这个滤镜的回调里面更新element
+    GPUImageFilter *filter = [[GPUImageFilter alloc]init];
+    [self.camera addTarget:filter];
+    [filter addTarget:blendFilter];
+    
+    [blendFilter addTarget:self.imageView];
+    
+    __weak typeof(self) weakSelf = self;
+    [filter setFrameProcessingCompletionBlock:^(GPUImageOutput *output, CMTime time) {
+        [weakSelf.element update];
+    }];
+    [self.camera startCameraCapture];
+    
 }
-
-
 
 @end
