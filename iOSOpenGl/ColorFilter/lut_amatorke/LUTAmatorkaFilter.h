8776e8c5657f4fe2fcd275a0d0f96cfb5dcb62d1
yongjian.wan <yongjian.wan@ecovacs.com>
2018-05-24 15:51:12 +0800

    封装滤镜组

---
+++ iOSOpenGl/ColorFilter/lut_amatorke/LUTAmatorkaFilter.h
@@ -0,0 +1,13 @@
+//
+//  LUTAmatorkaFilter.h
+//  iOSOpenGl
+//
+//  Created by wanyongjian on 2018/5/24.
+//  Copyright © 2018年 wan. All rights reserved.
+//
+
+#import "GPUImageFilterGroup.h"
+
+@interface LUTAmatorkaFilter : GPUImageFilterGroup
+
+@end
---
+++ iOSOpenGl/ColorFilter/lut_amatorke/LUTAmatorkaFilter.m
@@ -0,0 +1,13 @@
+//
+//  LUTAmatorkaFilter.m
+//  iOSOpenGl
+//
+//  Created by wanyongjian on 2018/5/24.
+//  Copyright © 2018年 wan. All rights reserved.
+//
+
+#import "LUTAmatorkaFilter.h"
+
+@implementation LUTAmatorkaFilter
+
+@end
